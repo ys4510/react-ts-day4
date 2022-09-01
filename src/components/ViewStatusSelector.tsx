de6261b61e90eac44a9a74b51358e3ac54cbfe82
ys4510 <ystaich@gmail.com>
2022-08-31 18:31:14 -0700

    first commit

---
+++ src/components/ViewStatusSelector.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ViewStatusSelector = () => {
+  return (
+    <>
+    <hr />
+    <label>
+      <span>View Options :</span>
+      <select>
+        <option>aaaa</option>
+        <option>bbb</option>
+      </select>
+    </label>
+    </>
+  )
+}
+
+export default ViewStatusSelector
